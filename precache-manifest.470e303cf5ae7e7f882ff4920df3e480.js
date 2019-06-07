689014524497c476a7a367752965032b453e78d8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-07 23:50:45 +0000

    Deploy iota-saarland/iota.saarland to github.com/iota-saarland/iota.saarland.git:gh-pages

---
+++ precache-manifest.470e303cf5ae7e7f882ff4920df3e480.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "82a7be8f913a6bfd24ce",
+    "url": "/js/about.26ba1cd8.js"
+  },
+  {
+    "revision": "4ac73b0d4d781579a83e",
+    "url": "/css/app.8f7e1d5b.css"
+  },
+  {
+    "revision": "4ac73b0d4d781579a83e",
+    "url": "/js/app.d853f444.js"
+  },
+  {
+    "revision": "a5bcfa71fde94ed418d0",
+    "url": "/css/chunk-vendors.24c39596.css"
+  },
+  {
+    "revision": "a5bcfa71fde94ed418d0",
+    "url": "/js/chunk-vendors.f6f2fdc1.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "30747c462887bd6008fdc3de10b1a3df",
+    "url": "/img/logo.30747c46.svg"
+  },
+  {
+    "revision": "f579b84c43bb7417fa510ec9f22abec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf340cae1b6b687150360d6ce9b14bc",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
